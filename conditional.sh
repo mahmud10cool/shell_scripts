04f16f7da93ab42c1eaa7074b91a08ec3270e5bd
mahmud10cool <mutashi@gmail.com>
2023-01-07 17:41:50 +0600

    Conditional statements are interesting

--- conditional.sh
+++ conditional.sh
@@ -4,7 +4,22 @@ NAME="Mahmud"
 
 if [ "$NAME" = "Mahmud" ];
 then
-	echo "Welcome Mahmud"
+	echo "Welcome $NAME"
 fi
 
-echo $NAME
+# One line empty for beauty reasons
+echo " "
+
+# Prompting the user
+echo "Please enter your username"
+read NAME
+
+# Now moving on to if-else statements
+
+if [ "$NAME" = "Mahmud" ];
+then
+	echo "Welcome back $NAME"
+else
+	echo "Invalid username, please register an account"
+fi
+
