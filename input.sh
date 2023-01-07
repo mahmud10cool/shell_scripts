3066b9e572dad608049fd7ee1170c93f8a462da8
mahmud10cool <mutashi@gmail.com>
2023-01-07 17:23:13 +0600

    Trying out multivariable inputs

--- input.sh
+++ input.sh
@@ -3,6 +3,14 @@
 # Everything becomes really intuitive as I go along
 # We will be touching on standard input
 
+# User Input
 read -p "Enter your name: " NAME
 
 echo "Oh my god your name is $NAME" 
+
+# Let us first give the user a prompt
+echo "Please enter your full name: "
+
+read FNAME LNAME
+
+echo "Wow you are $FNAME $LNAME"
