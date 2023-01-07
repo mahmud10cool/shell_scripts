75a5e486f673546a07d9c5b930e5a63823e21f45
mahmud10cool <mutashi@gmail.com>
2023-01-07 17:20:36 +0600

    Learning about inputs

---
+++ input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Everything becomes really intuitive as I go along
+# We will be touching on standard input
+
+read -p "Enter your name: " NAME
+
+echo "Oh my god your name is $NAME" 
