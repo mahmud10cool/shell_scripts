98e88145e2168092b773de415882e3e872a82375
mahmud10cool <mutashi@gmail.com>
2023-01-09 14:18:42 +0600

    Trying out the test.sh file

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 # The tests are designed to automate tasks that require conditional data
+
+if [ -d /usr/share/dict ];
+then
+	echo "Yes it exists"
+else
+	echo "The file does not exist"
+fi
