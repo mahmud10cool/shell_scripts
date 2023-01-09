8ef4ed85a79067bc24dc7a079a9698aa4cca26a7
mahmud10cool <mutashi@gmail.com>
2023-01-09 14:27:15 +0600

    Lecture 5 completed

--- test.sh
+++ test.sh
@@ -2,9 +2,20 @@
 
 # The tests are designed to automate tasks that require conditional data
 
-if [ -d /usr/share/dict ];
+# To test for a file we use -e
+if [ -e /etc/shadow ];
 then
 	echo "Yes it exists"
 else
 	echo "The file does not exist"
+fi
+
+echo "   "
+
+# To test for a directory we use the -d
+if [ -d /usr/share/dict ];
+then
+	echo "This directory exists"
+else
+	echo "This directory does not exist"
 fi
