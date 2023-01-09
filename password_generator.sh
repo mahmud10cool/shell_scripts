4a8daae377b0cc6d6e75b408914a7d4e9ddcfe31
mahmud10cool <mutashi@gmail.com>
2023-01-09 20:41:54 +0600

    Trying out other encryption algorithms

--- password_generator.sh
+++ password_generator.sh
@@ -18,8 +18,21 @@ echo " "
 echo "Please enter the length of the password:"
 read PASS_LENGTH
 
+echo "The base64 password: "
 for p in $(seq 1);
 do
-	# The code below is first generating a 48 character password using the base64 encryption method and then cutting it to the desired length
+	# The code below is first generating a 48 character password using the base64 encryption algortihm and then cutting it to the desired length
 	openssl rand -base64 48 | cut -c1-$PASS_LENGTH
 done
+
+echo " "
+
+# Just to see the difference between the base64 algorithm and other algorithms such as the hex
+echo "The hex password: "
+for x in $(seq 1);
+do
+	openssl rand -hex 48 | cut -c1-$PASS_LENGTH
+done
+
+echo " "
+echo "Note that there may be repeated or simple characters in the hex version of the password"
