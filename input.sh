b7e4fec59385b573646f412fb3831b6a396a4056
mahmud10cool <mutashi@gmail.com>
2023-01-10 17:53:08 +0600

    Finished with editing input.sh file

--- input.sh
+++ input.sh
@@ -14,7 +14,22 @@ echo "Please enter your class and age: "
 
 read CLASS AGE
 
-echo "Wow you are $CLASS and you are $AGE years old!"
+echo "Wow you are in class $CLASS and you are $AGE years old!"
 
+DIFF=6
+SHOULD_BE=$(($CLASS + $DIFF))
+ACTUAL=$(($AGE - $SHOULD_BE))
+
+echo " "
+
+if [ $ACTUAL -gt 0 ];
+then
+	echo "You should be $SHOULD_BE years old. But you are $ACTUAL years older"
+elif [ $ACTUAL -eq 0 ];
+then
+	echo "You are the right age for your grade"
+else
+	echo "Congratulations, you are quite precocious"
+fi
 # This one of the advantages of shell scripting
 # It is pretty versatile
